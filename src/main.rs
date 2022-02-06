311c8cf7b5490be4ad6647aff3bb3eac8508435d
Guilherme Augusto de Souza <lguist@protonmail.com>
2022-02-06 15:43:59 -0300

    Working black screen without warning or erros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::graphics::{self, Color};
+use ggez::event::{self, EventHandler};
+
+struct MainState {
+}
+
+impl MainState {
+    pub fn new(_ctx: &mut Context) -> MainState {
+        MainState {
+        }
+    }
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, Color::BLACK);
+        graphics::present(ctx)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (mut ctx, event_loop) = ContextBuilder::new("Rong", "lguist")
+        .build()
+        .expect("Could not create ggez context!");
+
+    graphics::set_window_title(&ctx, "Rong");
+    let state = MainState::new(&mut ctx);
+
+    event::run(ctx, event_loop, state);
 }
